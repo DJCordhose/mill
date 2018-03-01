a7b985ddf23c6921133cba8a655bacf0ffc30d1d
Jannis <zocktv1@gmail.com>
2018-03-01 15:00:33 +0100

    wip

---
+++ interaction.js
@@ -0,0 +1,20 @@
+const fields = document.querySelectorAll('span.field');
+fields.forEach(field => field.addEventListener('click', e => {
+    const fieldd = getFieldForId(e.target.id);
+    if (isSetAllowed(fieldd)) {
+        fieldd.state = game.player;
+        createBoard(data);
+    }
+}));
+
+function getFieldForId(id) {
+    return data.filter(function(dataa) {
+        return dataa.id == id;
+    })[0];
+}
+
+function isSetAllowed(field) {
+    if (field.state == "o") {
+        return true;
+    }
+}
