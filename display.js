9eec014cb86e49a98ffd28b3074657da3cacad03
Jannis <zocktv1@gmail.com>
2018-03-02 11:37:58 +0100

    wip

--- display.js
+++ display.js
@@ -20,30 +20,127 @@ ${fields.f71}-----------${fields.f74}-----------${fields.f77}`;
 }
 
 function handleClick(id, data, game) {
-    if (game.phase == "Set") {
+    if (game.phase === "Set") {
         const field = getFieldForId(data, id);
 
         if (field.state == "o") {
             setState(id, game.player);
 
-            if (game.player == 'B') {
-                game.bPiecesSettable--;
+            if (detectMill(id, game.player, data, game)) {
+                if (game.player == "B") {
+                    game.bPiecesSettable--;
+    
+                    if (game.bPiecesSettable == 0) {
+                        game.phase = "Moving Mill";
+                    } else {
+                        game.phase = "Set Mill";
+                    }
+                } else {
+                    game.wPiecesSettable--;
 
-                if (game.bPiecesSettable == 0) {
-                    game.phase = 'Moving'
+                    game.phase = "Set Mill";
                 }
-
-                game.player = whiteSymbol;
             } else {
-                game.wPiecesSettable--;
-                game.player = blackSymbol;
+                if (game.player == "B") {
+                    game.bPiecesSettable--;
+    
+                    if (game.bPiecesSettable == 0) {
+                        game.phase = "Moving";
+                    }
+    
+                    game.player = whiteSymbol;
+                } else {
+                    game.wPiecesSettable--;
+                    game.player = blackSymbol;
+                }
             }
 
             displayBoard(getData(), game);
         }
-    } else {
+    } else if (game.phase === "Set Mill") {
+        if (!detectMill(id, (game.player === "W" ? "B" : "W"), data, game) & getData()[idx4Id(id)].state != "o" & getData()[idx4Id(id)].state != game.player) {
+            setState(id, "o");
 
+            if (game.player == "B") {
+                game.wPiecesLost++;
+                game.player = whiteSymbol;
+            } else {
+                game.bPiecesLost++;
+                game.player = blackSymbol;
+            }
+
+            game.phase = "Set";
+
+            displayBoard(getData(), game);
+        }
+    } else if (game.phase === "Moving Mill") {
+        if (!detectMill(id, (game.player === "W" ? "B" : "W"), data, game) & getData()[idx4Id(id)].state != "o" & getData()[idx4Id(id)].state != game.player) {
+            setState(id, "o");
+
+            if (game.player == "B") {
+                game.wPiecesLost++;
+                game.player = whiteSymbol;
+            } else {
+                game.bPiecesLost++;
+                game.player = blackSymbol;
+            }
+
+            game.phase = "Moving";
+
+            displayBoard(getData(), game);
+        }
     }
+}
+
+function detectMill(id, player, data, game) {
+    fieldData = getData();
+    hMill = [fieldData[idx4Id(id)]];
+    vMill = [fieldData[idx4Id(id)]];
+
+    getData().forEach(function(d) {
+        dIdX = d.id.charAt(2);
+        dIdY = d.id.charAt(1);
+        idX = id.charAt(2);
+        idY = id.charAt(1);
+
+        if (dIdY != "4") {
+            if (idY == dIdY & id != d.id & d.state === player) {
+                hMill.push(d);
+            }
+        } else {
+            if (idX <= 3) {
+                if (idY == dIdY & id != d.id & dIdX <= 3 & d.state === player) {
+                    hMill.push(d);
+                }
+            } else {
+                if (idY == dIdY & id != d.id & dIdX >= 5 & d.state === player) {
+                    hMill.push(d);
+                }
+            }
+        }
+
+        if (dIdX != "4") {
+            if (idX == dIdX & id != d.id & d.state === player) {
+                vMill.push(d);
+            }
+        } else {
+            if (idY <= 3) {
+                if (idX == dIdX & id != d.id & dIdY <= 3 & d.state === player) {
+                    vMill.push(d);
+                }
+            } else {
+                if (idX == dIdX & id != d.id & dIdY >= 5 & d.state === player) {
+                    vMill.push(d);
+                }
+            }
+        }
+    });
+
+    if (hMill.length === 3 | vMill.length === 3) {
+        return true;
+    }
+
+    return false;
 }
 
 function displayBoard(data, game) {
@@ -64,7 +161,7 @@ function displayBoard(data, game) {
 }
 
 function getFieldForId(data, id) {
-    return data.filter(function(dataa) {
-        return dataa.id == id;
+    return data.filter(function(d) {
+        return d.id == id;
     })[0];
 }
