c3abb98dd00a6df5d7cb2deab690161616a75430
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2018-03-02 15:39:03 +0100

    better evaluation

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 const whiteSymbol = 'W';
 const blackSymbol = 'B';
+const emptySymbol = 'o';
 
 const phaseSet = 'Set';
 const phaseMoving = 'Moving';
@@ -7,14 +8,35 @@ const phaseMoving = 'Moving';
 let compressedState = "oooooooooooooooooooooooo";
 const ids = ['f11', 'f14', 'f17', 'f22', 'f24', 'f26', 'f33', 'f34', 'f35', 'f41', 'f42', 'f43', 'f45', 'f46', 'f47', 'f53', 'f54', 'f55', 'f62', 'f64', 'f66', 'f71', 'f74', 'f77'];
 const values = [
-     2, 3, 2,
-     2, 4, 2,
-     2, 3, 2,
-     3, 4, 3, 3, 4, 3,
-     2, 3, 2,
-     2, 4, 2,
-     2, 3, 2,
-     ];
+    2, 3, 2,
+    2, 4, 2,
+    2, 3, 2,
+    3, 4, 3, 3, 4, 3,
+    2, 3, 2,
+    2, 4, 2,
+    2, 3, 2,
+];
+
+const mills = [
+    // horizontal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [9, 10, 11], [12, 13, 14],
+    [15, 16, 17],
+    [18, 19, 20],
+    [21, 22, 23],
+
+    // vertical
+
+    [0, 9, 18],
+    [3, 10, 15],
+    [6, 11, 15],
+    [1, 4, 7], [16, 19, 22],
+    [8, 12, 17],
+    [5, 13, 20],
+    [2, 14, 23]
+];
 
 const game = {
     player: whiteSymbol,
@@ -46,7 +68,7 @@ function idx4Id(id) {
 
 function setState(id, state) {
     const idx = idx4Id(id);
-    const arr = [... compressedState];
+    const arr = [...compressedState];
     arr[idx] = state;
     const newState = arr.join('');
     compressedState = newState;
--- eval.js
+++ eval.js
@@ -1,3 +1,5 @@
+const millScore = 12;
+
 // black pos, white neg
 function evaluate(compressedState) {
     let scoreWhite = 0;
@@ -12,6 +14,36 @@ function evaluate(compressedState) {
         }
     });
 
+    const {
+        whiteMills,
+        blackMills
+    } = countMills(compressedState);
+
+    scoreBlack += blackMills * millScore;  
+    scoreWhite += whiteMills * millScore;  
+
     return scoreBlack - scoreWhite;
 }
 
+function countMills(compressedState) {
+    let whiteMills = 0;
+    let blackMills = 0;
+
+    mills.forEach(mill => {
+        const m0 = compressedState[mill[0]];
+        const m1 = compressedState[mill[1]];
+        const m2 = compressedState[mill[2]];
+        
+        if (m0 !== emptySymbol && m0 === m1 && m1 === m2) {
+            if (m0 === blackSymbol) {
+                blackMills++;
+            } else {
+                whiteMills++;
+            }
+        }
+    });
+    return {
+        whiteMills,
+        blackMills
+    };
+}
